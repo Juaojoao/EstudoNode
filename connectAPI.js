a70391fd2ede941b887a4f3f00de2d774430a9cf
Juaojoao <soloqf@gmail.com>
2023-03-06 10:40:27 -0300

    Modifi comment in the file

--- connectAPI.js
+++ connectAPI.js
@@ -7,6 +7,8 @@ API = 'https://jsonplaceholder.typicode.com/users?_limit=2';
 
 //GET é uma função do JavaScript que pega o conteúdo.
 https.get(API, resposta => {
+
+    // Pego a API e a resposta para obter um retorno usando o "statusCode"
     console.log(resposta.statusCode);
 })
 
